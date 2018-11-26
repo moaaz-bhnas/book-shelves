1aef2f6542ff01ae9f36f39a0114f154790acf4d
Moaaz <moaaz_bs@yahoo.com>
2018-11-26 18:56:47 +0200

    added header

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header role="banner">
+      <h1>
+        Book Shelves
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
