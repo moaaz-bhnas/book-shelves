39b71fbc07deb33a989dab0a46d63db8a4530d3f
Moaaz <moaaz_bs@yahoo.com>
2018-12-07 11:34:52 +0200

    Updates

---
+++ precache-manifest.fe2a0eeff7a13a19cf40795378b1bbfc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca7c3819ac6a3cafe43403b36b68b7ae",
+    "url": "/book-shelves/static/media/hp3.ca7c3819.jpg"
+  },
+  {
+    "revision": "165c2aa3ae3c1f266327",
+    "url": "/book-shelves/static/css/main.4f8f2366.chunk.css"
+  },
+  {
+    "revision": "bed2fc873117524cebac",
+    "url": "/book-shelves/static/js/1.bed2fc87.chunk.js"
+  },
+  {
+    "revision": "c98e1487fffd31e9af72",
+    "url": "/book-shelves/static/js/runtime~main.c98e1487.js"
+  },
+  {
+    "revision": "9d5cdaf71fb17d84e6a1874438e37f4b",
+    "url": "/book-shelves/static/media/hp1.9d5cdaf7.jpg"
+  },
+  {
+    "revision": "6c46e0adc5e4412218376dfd0381cba4",
+    "url": "/book-shelves/static/media/hp2.6c46e0ad.jpg"
+  },
+  {
+    "revision": "165c2aa3ae3c1f266327",
+    "url": "/book-shelves/static/js/main.165c2aa3.chunk.js"
+  },
+  {
+    "revision": "d00f8fb69a91cce9f4ceaf8956fc095b",
+    "url": "/book-shelves/static/media/hp4.d00f8fb6.jpg"
+  },
+  {
+    "revision": "a194e3e75a54e46aebcbfdd21380468e",
+    "url": "/book-shelves/static/media/hp5.a194e3e7.jpg"
+  },
+  {
+    "revision": "503c6ee7945b7db56ca732fa72b9c247",
+    "url": "/book-shelves/static/media/hp6.503c6ee7.png"
+  },
+  {
+    "revision": "0041fe33f2a71bd44632fd1e21f74b81",
+    "url": "/book-shelves/static/media/hp7.0041fe33.jpg"
+  },
+  {
+    "revision": "6edf3e7242cd84b5445c4198887f5412",
+    "url": "/book-shelves/index.html"
+  }
+];
