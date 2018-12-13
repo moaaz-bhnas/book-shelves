3d324b9e2864869f4d95b1b1401ea6eaa3e6d31b
Moaaz <moaaz_bs@yahoo.com>
2018-12-13 16:04:20 +0200

    Updates

--- precache-manifest.0c9a29c05d8e41e9a79d6166ac3f8de7.js
+++ precache-manifest.0c9a29c05d8e41e9a79d6166ac3f8de7.js
@@ -1,35 +1,31 @@
 self.__precacheManifest = [
   {
-    "revision": "6c46e0adc5e4412218376dfd0381cba4",
-    "url": "/book-shelves/static/media/hp2.6c46e0ad.jpg"
+    "revision": "ca7c3819ac6a3cafe43403b36b68b7ae",
+    "url": "/book-shelves/static/media/hp3.ca7c3819.jpg"
   },
   {
-    "revision": "15a28481748d795ab960",
-    "url": "/book-shelves/static/css/main.ab4b14f6.chunk.css"
+    "revision": "875c5611ef288747ee81",
+    "url": "/book-shelves/static/css/main.2b99b008.chunk.css"
   },
   {
-    "revision": "591600e29e2b3377f35b",
-    "url": "/book-shelves/static/js/1.591600e2.chunk.js"
+    "revision": "e079918060f260a4adb6",
+    "url": "/book-shelves/static/js/1.e0799180.chunk.js"
   },
   {
     "revision": "c98e1487fffd31e9af72",
     "url": "/book-shelves/static/js/runtime~main.c98e1487.js"
   },
   {
-    "revision": "73d502ef09f9c1b5019355f13fbba3cd",
-    "url": "/book-shelves/static/media/smile.73d502ef.svg"
-  },
-  {
     "revision": "9d5cdaf71fb17d84e6a1874438e37f4b",
     "url": "/book-shelves/static/media/hp1.9d5cdaf7.jpg"
   },
   {
-    "revision": "15a28481748d795ab960",
-    "url": "/book-shelves/static/js/main.15a28481.chunk.js"
+    "revision": "6c46e0adc5e4412218376dfd0381cba4",
+    "url": "/book-shelves/static/media/hp2.6c46e0ad.jpg"
   },
   {
-    "revision": "ca7c3819ac6a3cafe43403b36b68b7ae",
-    "url": "/book-shelves/static/media/hp3.ca7c3819.jpg"
+    "revision": "875c5611ef288747ee81",
+    "url": "/book-shelves/static/js/main.875c5611.chunk.js"
   },
   {
     "revision": "d00f8fb69a91cce9f4ceaf8956fc095b",
@@ -48,7 +44,7 @@ self.__precacheManifest = [
     "url": "/book-shelves/static/media/hp7.0041fe33.jpg"
   },
   {
-    "revision": "0530b4fa3332723ab83d8f774f692780",
+    "revision": "8343042c6f982a2d58e27406ddfa5b6e",
     "url": "/book-shelves/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/book-shelves/precache-manifest.08410b17b9ffb0ad19a3b008d19b0d9d.js"
+  "/book-shelves/precache-manifest.0c9a29c05d8e41e9a79d6166ac3f8de7.js"
 );
 
 workbox.clientsClaim();
